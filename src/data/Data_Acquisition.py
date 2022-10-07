66e9d49c1c6f6b27a24b3f354a62b99a1b159013
Yashaswi <pupneja.yashu@gmail.com>
2022-10-07 17:40:46 -0400

    Ipywidgets.ipynb

--- src/data/Data_Acquisition.py
+++ src/data/Data_Acquisition.py
@@ -53,143 +53,131 @@
 #     get_game_datas(year,'./data/{}_game_data.json'.format(year))
 
 
-import time
+import os, sys
+import argparse
 import requests
 import json
-import os
+import time
 
 """
-references:
-Download_Data: https://www.askpython.com/python/examples/pull-data-from-an-api 
+The user passes as command line argument:
+    The seasons to download: from_year - to_year (where to_year is optional) 
+
+    A directory where the data files will be stored
+
 """
 
-## Author: @Rachel
-def download_seasonal_data(season, path, game_type):
-    """download data of a whole season"""
 
-    if game_type == 'regular_season': # initialize
-        prev_game_number = '0000'
-    elif game_type == 'playoff':
-        prev_game_number = '0110'
-    prev_game_available = True
-    game_id = '0000000000'
-
-    while (game_id != 'end'):
-        game_id = seasonal_game_id(season, game_type, prev_game_number, prev_game_available)
-        prev_game_available = True
-        # print(game_id)
-        url = 'https://statsapi.web.nhl.com/api/v1/game/{game_id}/feed/live/'.format(game_id=game_id)
-        parse_json = download_data(url)
-        if (len(parse_json.keys()) == 2):  # url with the given game_id not available
-            prev_game_available = False
-
-        else:
-            filtered_json = parse_json
-            write_to_file(filtered_json, game_id, path)
-        prev_game_number = game_id[6:]
+def get_data_regular(year, file_path):
+    """
+    The function takes as input year representing a season and a file path
+    and downloads all the play_by_play events of the regular season
+    in file_path. 
 
 
-def download_data(url):
-    """Download the raw data from the link passed as arg"""
 
-    response_api = requests.get(url)
-    data = response_api.text
-    parse_json = json.loads(data)
-    # print(parse_json.keys())
+    """
+    start = time.time()
 
-    return parse_json
-
-
-def seasonal_game_id(season, game_type, prev_game_number, prev_game_available):
-    """returns next possible game id of a chosen season"""
-
-    playoff_id = '03'
-    regual_season_id = '02'
-
-    if game_type == 'regular_season':
-        game_type_id = regual_season_id
-        game_number = str(int(prev_game_number) + 1)
-        game_number = "0"*(4-len(game_number)) + game_number
-        if prev_game_available == False:
-            game_id = 'end'
-            return game_id
-
-    elif game_type == 'playoff':
-        game_type_id = playoff_id
-        new_game_number_status, game_number = new_playoff_game_number(prev_game_number, prev_game_available)
-        if new_game_number_status == 'end':
-            return new_game_number_status
-
-    game_id = season + game_type_id + game_number
-    # print(game_id)
-    return game_id
-
-
-def new_playoff_game_number(prev_game_number, prev_game_available):
-    """returns the last 4 digits of a playoff game id, according to the last playoff id used to download"""
-
-    new_game_number = prev_game_number
-    # print(prev_game_available)
-    if prev_game_available:
-        if prev_game_number[3] == '7': # last game. we should go to the next matchup
-            new_game_number = change_string(new_game_number, 2, 4, str(int(new_game_number[2:]) + 4)) # increase the matchup and set the game to 1
-            new_game_number_status, new_game_number = check_new_game_number(new_game_number)
-        else: #not the last game. We should go to the next game
-            new_game_number = change_string(new_game_number, 3, False, str(int(new_game_number[3]) + 1))
-            # print(new_game_number)
-            new_game_number_status = 'continue'
-    else: #if prev number was unavailable
-        new_game_number = change_string(new_game_number, 2, 4, str(int(new_game_number[2:]) + 10)) # So we should go on to the next matchup
-        new_game_number = change_string(new_game_number, 3, False, '1')
-        new_game_number_status, new_game_number = check_new_game_number(new_game_number)
-        print("im here")
-    return new_game_number_status, new_game_number
-
-
-def check_new_game_number(new_game_number):
-    # print(new_game_number[1:3])
-    if new_game_number[1:3] == '42':  # check the 2nd and 3rd digits of the last 4 digits
-        new_game_number_status = 'end'  # end of last(4th) round
-    elif new_game_number[1:3] == '33':  # end of 3rd round
-        new_game_number_status = 'continue'
-        new_game_number = change_string(new_game_number, 1, 4, '411')
-    elif new_game_number[1:3] == '25':  # end of 2nd round
-        # print("yea")
-        new_game_number_status = 'continue'
-        new_game_number = change_string(new_game_number, 1, 4, '311')
-    elif new_game_number[1:3] == '19':  # end of 1st round
-        new_game_number_status = 'continue'
-        new_game_number = change_string(new_game_number, 1, 4, '211')
+    #if the file already exists, the function just loads it
+    if os.path.isfile(file_path):
+        with open(file_path, "r") as f_regular: 
+            json.load(f_regular)
+            return
+    
     else:
-        new_game_number_status = 'continue'
-    return new_game_number_status, new_game_number
 
 
-def change_string(string, index_start, index_end, new_char):
-    string_list = list(string)
-    # new_char_list = list(new_char)
-    if index_end == False:
-        string_list[index_start] = new_char
-    else:
-        string_list[index_start:index_end] = new_char
-    new_string = "".join(string_list)
-    # print(new_string)
-    return new_string
+        #iterating through all the games until we reach a game with no data (game with no 'liveData')
+        
+        all_regular_games = {} #dictionary (key, value) = (ID, game_data)
+        
+        game_exists = True #while the url contains information about the game
+        i = 1 
+
+        #This loop will go through all ids from 1 to the number of games  
+        while game_exists == True:
+            GAME_ID = "{}02{:04d}".format(year, i)
+            url = 'https://statsapi.web.nhl.com/api/v1/game/{}/feed/live/'.format(GAME_ID)
+            r = requests.get(url).json() #request all data about the game
+            regular_game = {} 
+            if r.get('liveData') is not None: #if 'liveData' == None, then no data for that game.
+                regular_game['gameData'], regular_game['liveData'] = r['gameData'],r['liveData']
+                all_regular_games[GAME_ID] = regular_game
+                i +=1
+            else:
+                game_exists = False
+
+        #write the json file: 
+        with  open(file_path, 'w') as f_regular:
+            json.dump(all_regular_games, f_regular)
+        end = time.time()
+        print("time for regular season", end - start)
+        return 
+ 
+#Playoffs
+def get_data_playoffs(year, file_path):
+    """ The function takes a year representing a season and a file path
+    and downloads all the play_by_play events of the regular season
+    in file_path.
+    """
+    
+    start = time.time()
+
+    #if the file has already been downloaded
+    if os.path.isfile(file_path):
+        with open(file_path, "r") as f_playoffs: 
+            json.load(f_playoffs)
+        return 
+
+    
+    else: 
+        all_playoff_games = {} #stores the data for all games
+
+        #This loop iterates through all the possible IDs of a playoff
+        for p_round in range(1, 5): #4rounds
+            for match_up in range(1, 2**(4-p_round)+1): #2^(4-round) matchups in each round 
+                for game in range(1,8): #7 games for each matchup
+                    GAME_ID = '{}030{}{}{}'.format(year, p_round, match_up, game)
+                    url = 'https://statsapi.web.nhl.com/api/v1/game/{}/feed/live/'.format(GAME_ID)
+                    r = requests.get(url).json()
+                    playoff_game = {} 
+                    if r.get('liveData') is not None: #if the game has no data / no play_to_play data
+                        playoff_game['gameData'], playoff_game['liveData'] = r['gameData'],r['liveData']
+                        all_playoff_games[GAME_ID] = playoff_game
+
+        
+        with open(file_path, 'w') as f_playoff:
+            json.dump(all_playoff_games, f_playoff)
+            
+        end = time.time()
+        print("time for playoff season", end - start)
+        return 
+
+def main():
+    #This firt part (until the loop) can be removed if we don't need command line arguments" 
+    #Have to specify: year, to_year and path to the folder where the files will be downloaded
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument('-y', '--year', type=int, required=True)
+    # parser.add_argument('-p', '--path', type=str, required=True)
+    # parser.add_argument('-ty', '--to_year', type=int)
+    # args = parser.parse_args()
+
+    # year, path = args.year, args.path
+    # to_year = year 
+    # if len(sys.argv) == 7:
+    #     to_year = args.to_year
+    year = 2016
+    to_year = 2020
+    path = "/home/yashaswi/Documents/IFT 6758/Project NHL"
+     
+    for y in range(year, to_year+1): 
+        #file_path_regular, file_path_playoff = os.path.join(path, str(year) + "_regular_season.json"), os.path.join(path, str(year) + "_playoffs.json"),
+        file_path_regular, file_path_playoff = path + "/" + str(y) + "_regular_season.json", path + "/" + str(y) + "_playoffs.json"
+        get_data_playoffs(y, file_path_playoff) 
+        get_data_regular(y, file_path_regular)
 
 
-def write_to_file(parse_json, game_id, path):
-    """writes a dictionary(json data) to a json file"""
 
-    if not os.path.exists(path): #create path
-        os.makedirs(path)
-    path = path + game_id + ".json"
-    if not os.path.exists(path):
-        with open(path, "w") as outfile:
-            json.dump(parse_json, outfile)
-    print(game_id)
-
-
-start = time.time()
-download_seasonal_data('2020', 'raw_data/2020/', 'playoff')
-end = time.time()
-print("time is: ", end - start, " (s)")
+if __name__ == "__main__":
+    main()
