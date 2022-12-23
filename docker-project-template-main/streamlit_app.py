db4ac22b7f9f4281d1d459b9b65f4c2288ddaa44
Yashaswi <pupneja.yashu@gmail.com>
2022-12-22 22:37:51 -0500

    Part 4- Streamlit App UI

--- docker-project-template-main/streamlit_app.py
+++ docker-project-template-main/streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import plotly.express as px
 
 """
 General template for your streamlit app. 
@@ -8,20 +9,25 @@ Feel free to experiment with layout and adding functionality!
 Just make sure that the required functionality is included as well
 """
 
-st.title("INSERT APP TITLE HERE")
+st.title("Hockey Visualization App")
 
 with st.sidebar:
-    # TODO: Add input for the sidebar
-    pass
+    # Dropdown for Workspace selection
+    workspace = st.selectbox("Workspace", ["rachel98"])
+    # Dropdown for Model selection
+    model = st.selectbox("Model", ["XGBoost 1", "XGBoost 2", "XGBoost 3"])
+    # Dropdown for Version selection
+    version = st.selectbox("Version", ["v1", "v2", "v3"])
 
 with st.container():
-    # TODO: Add Game ID input
-    pass
+    # Game ID as dropdown
+    game_id = st.selectbox("Game ID", ["1", "2", "3"])
+    # Add button to load data
+    load_data = st.button("Ping Game")
+    # Display game data if button is pressed
+    if load_data:
+        st.write("Game data")
 
-with st.container():
-    # TODO: Add Game info and predictions
-    pass
 
-with st.container():
-    # TODO: Add data used for predictions
-    pass
+
+
