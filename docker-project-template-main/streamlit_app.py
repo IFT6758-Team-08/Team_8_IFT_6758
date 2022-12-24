8380cb370b9a1edaf2bd9f1db40aac39c370a21f
Rahil Ebrahimi <44610027+RahilEbrahimi98@users.noreply.github.com>
2022-12-24 16:29:49 -0500

    Update streamlit_app.py

--- docker-project-template-main/streamlit_app.py
+++ docker-project-template-main/streamlit_app.py
@@ -5,7 +5,8 @@ import plotly.express as px
 import ift6758.ift6758.client.game_client as gc
 from ift6758.ift6758.client.serving_client import ServingClient
 import json
-
+from comet_ml import API
+import os
 
 """
 General template for your streamlit app. 
@@ -17,15 +18,18 @@ sc = ServingClient()
 
 st.title("Hockey Visualization App")
 
-model = 'xgb3'
+model = 'xgb2'
+# model_ver = {'xgb3':["1.0.2", "1.0.1", "1.0.0"], 'xgb2':['1.0.0'], 'xgb1':['1.0.0']}
+api = API(api_key = os.environ.get('COMET_API_KEY'))
 
 with st.sidebar:
     # Dropdown for Workspace selection
     workspace = st.selectbox("Workspace", ["rachel98"])
     # Dropdown for Model selection
-    m = st.selectbox("Model", ["xgb3"])
+    m = st.selectbox("Model", ["xgb2", 'xgb3'])
+    ver = api.get_registry_model_versions(workspace, m)
     # Dropdown for Version selection
-    version = st.selectbox("Version", ["1.0.2", "1.0.1", "1.0.0"])
+    version = st.selectbox("Version", ver)
     get_model = st.button("Get Model")
     # print(model)
     if get_model:
@@ -36,8 +40,8 @@ with st.sidebar:
         # print(a)
         st.write("model downloaded")
 
-with st.container():
 
+with st.container():
     # Game ID as dropdown
     # game_id = st.selectbox("Game ID", ["2021020329", "2", "3"])
     game_id = st.text_input('Game ID', "2021020329")
@@ -47,16 +51,18 @@ with st.container():
 
     # Display game data if button is pressed
     if load_data:
+        st.write(game_id)
         ping = gc.ping_game_client(game_id)
         # st.write("Game data")
         res, away, home, cur_period, remaining_time, score = ping
-        st.write("Away Team: ", away)
-        st.write("Home Team: ", home)
-        st.write("Current Period: ", str(cur_period))
-        st.write("Remaining Time: ", remaining_time)
-        st.text("Scores: ")
-        st.write("away: ", str(score['away']))
-        st.write("home: ", str(score['home']))
+        # st.write("Away Team: ", away)
+        # st.write("Home Team: ", home)
+        st.subheader(home+"(home) VS "+away+"(away)")
+        st.write("Period ", str(cur_period), " - ", remaining_time, " left")
+        # st.write("Remaining Time: ", remaining_time)
+        # st.text("Scores: ")
+        # st.write("away: ", str(score['away']))
+        # st.write("home: ", str(score['home']))
         # st.write(res)
 
         print("model is", model)
@@ -72,12 +78,26 @@ with st.container():
             #                 'rebound', 'angle_change', 'speed', 'last_event_coordinates_x', 'last_event_coordinates_y',
             #                 'goal']
             #     df_to_predict = res[features]
-            # elif model == 'xgb1':
-            #     df_to_predict = res[:]
+            elif model == 'xgb2':
+                features = ['period', 'period_time', 'coordinates_x', 'coordinates_y',
+       'shot_distance', 'shot_angle', 'time_from_last_event(s)',
+       'distance_from_last_event', 'rebound', 'angle_change', 'speed',
+       'last_event_coordinates_x', 'last_event_coordinates_y', 'goal',
+       'secondary_type_Deflected', 'secondary_type_Slap Shot',
+       'secondary_type_Snap Shot', 'secondary_type_Tip-In',
+       'secondary_type_Wrap-around', 'secondary_type_Wrist Shot',
+       'last_event_type_Faceoff', 'last_event_type_Giveaway',
+       'last_event_type_Goal', 'last_event_type_Hit',
+       'last_event_type_Missed Shot', 'last_event_type_Penalty',
+       'last_event_type_Shot', 'last_event_type_Takeaway']
 
-            # print(df_to_predict)
+                teams = res['team']
+                df_to_predict = res[features]
+
+                # st.write(df_to_predict)
 
             predicted = sc.predict(df_to_predict)
+            st.write(predicted)
             predicted = json.loads(predicted)
             # st.write(predicted)
 
@@ -92,8 +112,23 @@ with st.container():
             away_prob = sum(df_with_teams[df_with_teams['team'] == away]['probability'])
             home_prob = sum(df_with_teams[df_with_teams['team'] == home]['probability'])
 
-            st.write("home team xG so far: ", home_prob, 'diff: ', home_prob - score['home'])
-            st.write("away team xG so far: ", away_prob, 'diff: ', away_prob - score['away'])
+            # st.write(home_prob, 'diff: ', home_prob - score['home'])
+            with open("tracker.json", 'r') as file:
+                prev_prob = json.load(file)
+                prev_home_prob = prev_prob[game_id]['home_prob']
+                prev_away_prob = prev_prob[game_id]['away_prob']
+
+
+            col1, col2 = st.columns(2)
+            # st.write(round(home_prob, 2))
+            col1.metric(label=home+" xG (actual)", value=str(round(home_prob + prev_home_prob, 2)) + "(" +str(score['home']) + ")", delta=round(home_prob - score['home'], 2))
+            col2.metric(label=away+" xG (actual)", value=str(round(away_prob + prev_away_prob, 2)) + "(" +str(score['away']) + ")", delta=round(away_prob - score['away'], 2))
+
+            prev_prob[game_id]['home_prob'] = round(home_prob + prev_home_prob, 2)
+            prev_prob[game_id]['away_prob'] = round(away_prob + prev_away_prob, 2)
+            with open("tracker.json", 'w') as file:
+                json.dump(prev_prob, file)
+
             st.text("data used for prediction and probability")
             st.write(df_to_predict)
 
